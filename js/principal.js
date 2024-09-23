b44989df4111507f9d8a004e81734f426268728a
kadyjapb <163013058+kadyjapb@users.noreply.github.com>
2024-09-23 11:51:43 -0300

    fim

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,9 @@
 
 var titulo = document.querySelector(".titulo");
 titulo.textContent = " Aparecida nutricionista ";
+
+var paulo = document.querySelector('#linhaPaulo');
+var pesoPaulo = paulo.querySelector(".info-peso").textContent;
+var alturaPaulo = paulo.querySelector(".info-altura").textContent;
+var imcPaulo = pesoPaulo / (alturaPaulo * alturaPaulo);
+paulo.querySelector(".info-imc").textContent = imcPaulo;
