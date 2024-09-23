31197b240132548970f54b00d3b07083df719fbd
kadyjapb <163013058+kadyjapb@users.noreply.github.com>
2024-09-23 12:10:17 -0300

    imc todos

--- js/principal.js
+++ js/principal.js
@@ -2,8 +2,19 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = " Aparecida nutricionista ";
 
-var paulo = document.querySelector('#linhaPaulo');
-var pesoPaulo = paulo.querySelector(".info-peso").textContent;
-var alturaPaulo = paulo.querySelector(".info-altura").textContent;
-var imcPaulo = pesoPaulo / (alturaPaulo * alturaPaulo);
-paulo.querySelector(".info-imc").textContent = imcPaulo;
+// var paulo = document.querySelector('#linhaPaulo');
+// var pesoPaulo = paulo.querySelector(".info-peso").textContent;
+// var alturaPaulo = paulo.querySelector(".info-altura").textContent;
+// var imcPaulo = pesoPaulo / (alturaPaulo * alturaPaulo);
+// paulo.querySelector(".info-imc").textContent = imcPaulo;
+
+var pacientes = document.querySelectorAll(".paciente");
+for (var i = 0; i < pacientes.length; i++){
+
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    var tdImc = paciente.querySelector(".info-imc");
+}
